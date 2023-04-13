f9624e897f45f5f9aa691e77c6475746371d5af3
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-04-13 18:21:19 +0300

    added module styles to profile

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -11,10 +11,16 @@ export const TransactionHistory = ({ transactions }) => {
 
       <tbody>
         {transactions.map(transaction => (
-          <tr key={transaction.id} id={transaction.id}>
-            <td>{transaction.type}</td>
-            <td>{transaction.amount}</td>
-            <td>{transaction.currency}</td>
+          <tr
+            key={transaction.id}
+            id={transaction.id}
+            className="transaction-history__row"
+          >
+            <td className="transaction-history__cell">{transaction.type}</td>
+            <td className="transaction-history__cell">{transaction.amount}</td>
+            <td className="transaction-history__cell">
+              {transaction.currency}
+            </td>
           </tr>
         ))}
       </tbody>
