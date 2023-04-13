b2d642d0ba35d2aee79ba285ce368465452d4e02
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-04-13 15:14:44 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,23 @@
 export const TransactionHistory = ({ transactions }) => {
-  return <div>TransactionHistory</div>;
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id} id={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
