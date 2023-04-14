f1442ec40d86db0352f1e956a36593aae2f8d018
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-04-14 15:58:12 +0300

    all done, propTypes left

--- src/components/FriendList/FriendCard.jsx
+++ src/components/FriendList/FriendCard.jsx
@@ -3,7 +3,12 @@ import css from './friendList-styles.module.css';
 export const FriendCard = ({ friend: { avatar, name, isOnline } }) => {
   return (
     <>
-      <span className={css.status}>{isOnline}</span>
+      {isOnline ? (
+        <span className={css.isOnline}></span>
+      ) : (
+        <span className={css.isOffline}></span>
+      )}
+
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </>
